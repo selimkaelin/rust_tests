e776315276ade6cae85e4aa7f257f550e4e14ea4
selimkaelin <selimkaelin@hotmail.com>
2021-01-28 07:50:29 +0100

    Started on struct declaration examples.

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,8 @@
 // mod csv_reader;
-extern crate csv;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-
-use std::env;
-use std::error::Error;
-use std::ffi::OsString;
-use std::fs::File;
-use std::process;
-
-// derive Debug, what for?
-#[derive(Debug, Deserialize)]
-
-
-fn run() -> Result<(), Box<Error>> {
-    let file_path = get_first_arg()?;
-    let mut rdr = csv::ReaderBuilder::new()
-        .delimiter(b';')
-        .from_path(file_path)?;
-    for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
-    }
-    Ok(())
-}
-
-fn get_first_arg() -> Result<OsString, Box<Error>> {
-    match env::args_os().nth(1) {
-        None => Err(From::from("Expected 1 argument, but got none.")),
-        Some(file_path) => Ok(file_path),
-    }
-}
-
-// fn read_csv() -> Result<Vec<T>, Box<Error>> {
-//     let file_path = get_first_arg()?;
-//     let mut data = vec![vec![0.0; 8]; 1];
-//     let mut reader = csv::ReaderBuilder::new()
-//         .delimiter(b';')
-//         .from_path(file_path)?;
-//     for result in reader.records() {
-//         let record= result?;
-//         data.push(record);
-//     }
-//
-//     Ok(data)
-// }
+// mod csv;
+mod matrix_product;
 
 fn main() {
-    if let Err(err) = run() {
-        println!("{}", err);
-        process::exit(1);
-    }
+    matrix_product::run();
 }
 
-// fn run() -> Result<(), Box<Error>> {
-//     let mut rdr = csv::Reader::from_reader(io::stdin());
-//     for result in rdr.records() {
-//         let record = result?;
-//         println!("{:?}", record);
-//         // match result {
-//         //     Err(err) => return Err(From::from(err)),
-//         //     Ok(record) => {
-//         //         println!("{:?}", record);
-//         //     }
-//         // }
-//     }
-//     Ok(())
-// }
---
+++ src/matrix_product.rs
@@ -0,0 +1,25 @@
+// defines the product of two matrices
+
+//TODO: define struct for matrix
+//TODO: define functions for matrix manipulation
+
+pub struct Matrix {
+    n_rows: i32,
+    n_cols: i32,
+    n_elements: i64,
+    data: Vec<f32>,
+}
+
+impl Matrix {
+    fn new(rows: i32, cols: i32) -> Matrix {
+
+    }
+}
+
+pub fn run() {
+    let mut m: Matrix;
+
+    println!("{}", m.n_elements);
+
+
+}
